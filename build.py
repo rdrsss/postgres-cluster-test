60daf0f480d3f1d0677faaef1c15d752c216a8bb
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2016-10-21 23:14:12 +0000

    add create and delete docker image methods

--- build.py
+++ build.py
@@ -7,6 +7,21 @@
 #
 import os, getopt, sys, subprocess, json, time, ConfigParser
 
+IMAGE_NAME = "test_postgres"
+
+
+# Build docker image.
+def build_postgres_image():
+    print " Building postgres image ..."
+    os.chdir("postgres")
+    subprocess.call(["docker", "build", "-t", IMAGE_NAME, "."])
+    os.chdir("..")
+    print "done."
+
+# Delete docker image.
+def delete_image():
+    subprocess.call(["docker", "rmi", IMAGE_NAME])
+
 def usage():
     usage = """\
     --help          : Print out this usage text.
@@ -34,3 +49,18 @@ if __name__ == '__main__':
     if len(opts) == 0:
         usage()
         sys.exit(2)
+
+    # Check options
+    for o, a in opts:
+        if o in ("--help"):
+            usage()
+        if o in ("--image"):
+            for a in args:
+                if a in ("build"):
+                    build_postgres_image()
+                elif a in ("delete"):
+                    delete_image()
+                else:
+                    print "Provide arg, [build] or [delete]"
+
+
